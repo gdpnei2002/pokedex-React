85669f2a5ddf37c16ff89bca9f4bc35c3c312002
neilson <joseneilson25@gmail.com>
2022-03-11 02:58:03 -0300

    paginação

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import Navbar from './components/Navbar';
 import Searchbar from './components/Searchbar';
 
 function App() {
-
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
   const [pokemons, setPokemons] = useState([]);
 
+  const itensPerPage = 25
   const fetchPokemons = async () => {
     try {
       setLoading(true)
-      const data = await getPokemons();
+      const data = await getPokemons(itensPerPage, itensPerPage * page);
       const promises = data.results.map(async (pokemon) =>{
         return await getPokemonData(pokemon.url)
       });
@@ -21,21 +23,27 @@ function App() {
       const results = await Promise.all(promises);
       setPokemons(results);
       setLoading(false);
+      setTotalPages(Math.ceil(data.count / itensPerPage))
      } catch (error) {
       console.log("fetchPokemons error: ", error);
      }
   }
 
   useEffect(() =>{
-    console.log("carregou")
     fetchPokemons();
-  }, [])
+  }, [page])
   
   return (
     <div>
       <Navbar />
       <Searchbar />
-      <Pokedex pokemons={pokemons} loading={loading}/>
+      <Pokedex 
+        pokemons={pokemons} 
+        loading={loading} 
+        page={page} 
+        setPage={setPage}
+        totalPages={totalPages}
+        />
     </div>
   );
 }
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, {useContext} from "react";
 
 const Pokemon = (props) => {
-    const {pokemon} = props
-    
-    return(
+    const {pokemon} = props;
+    const onHeartClick = () => {
+    }
+    return (
     <div className="pokemon-card">
-        <div className="pokemon-image-container"> 
-        <img alt={pokemon.name} src={pokemon.sprites.front_default} className="pokemon-image"/>
+        <div className="pokemon-image-container">
+            <img alt={pokemon.name} src={pokemon.sprites.front_default} className="pokemon-image"/>
         </div>
-        {pokemon.name}
+        <div className="card-body">
+            <div className="card-top">
+                <h3> {pokemon.name}</h3>
+                <div>#{pokemon.id}</div>
+            </div>
+            <div className="card-bottom">
+                <div className="pokemon-type">
+                    {pokemon.types.map((type, index) => {
+                        return (
+                            <div key={index} className="pokemon-type-text">{type.type.name}</div>
+                        )
+                    })}
+                </div>
+                <button className="pokemon-heart-btn" onClick={onHeartClick}>❤
+                </button>
+            </div>
+        </div>
     </div>)
 }
+
 export default Pokemon;
