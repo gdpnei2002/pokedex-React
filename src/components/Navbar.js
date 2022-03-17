0a167059e40e1fd391739d1bfe4e48bd3abe3c50
neilson <joseneilson25@gmail.com>
2022-03-17 20:43:42 -0300

    melhorias pessoais

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,18 @@ import FavoriteContext from "../context/favoritesContext";
 
 const Navbar = () => {
     const{favoritePokemons } = useContext (FavoriteContext);
-        const logoImg = "https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+        const logoImg = "https://crisgon.github.io/pokedex/src/images/logo.png"
+        const logobalImg = "https://icon-library.com/images/pokedex-icon/pokedex-icon-13.jpg"
     return(
         <nav>
             <div>
                 <img src={logoImg}
                 className="navbar-img"
                 />
+                <a href="#">
+                <img src={logobalImg}
+                className="logobalImg"
+                /></a>
             </div>
             <div>{favoritePokemons.length} 🧡</div>
         </nav>
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -9,28 +9,29 @@ const Pokemon = (props) => {
     }
     const heart = favoritePokemons.includes(pokemon.name) ? "❤️" : "🖤";
     return (
-    <div className="pokemon-card">
-        <div className="pokemon-image-container">
-            <img alt={pokemon.name} src={pokemon.sprites.front_default} className="pokemon-image"/>
-        </div>
-        <div className="card-body">
-            <div className="card-top">
-                <h3> {pokemon.name}</h3>
-                <div>#{pokemon.id}</div>
+    <div className="pokemon-card"><div>
+            <div className="pokemon-image-container">
+                <img alt={pokemon.name} src={pokemon.sprites.front_default} className="pokemon-image"/>
             </div>
-            <div className="card-bottom">
-                <div className="pokemon-type">
-                    {pokemon.types.map((type, index) => {
-                        return (
-                            <div key={index} className="pokemon-type-text">{type.type.name}</div>
-                        )
-                    })}
+            <div className="card-body">
+                <div className="card-top">
+                    <h3> {pokemon.name}</h3>
+                    <div>#{pokemon.id}</div>
                 </div>
-                <button className="pokemon-heart-btn" onClick={onHeartClick}>
-                      {heart}
-                </button>
+                <div className="card-bottom">
+                    <div className="pokemon-type">
+                        {pokemon.types.map((type, index) => {
+                            return (
+                                <div key={index} className="pokemon-type-text">{type.type.name}</div>
+                            )
+                        })}
+                    </div>
+                    <button className="pokemon-heart-btn" onClick={onHeartClick}>
+                        {heart}
+                    </button>
+                </div>
             </div>
-        </div>
+            </div>
     </div>)
 }
 
